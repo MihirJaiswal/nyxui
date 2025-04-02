0f9fdffa908e0b6ed775fe76d818ce6ee65adfed
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-04-02 21:43:53 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Header from "@/components/global/Header";
 import Hero from "@/components/home/Hero";
-import { AnimationTextDemo } from "@/nyxui/demos/AnimatedTextDemo";
 
 export default function Home() {
   return (
     <>
     <Header/>
-     <Hero/>
-     <AnimationTextDemo/>
+     <Hero/>\
     </>
   );
 }
