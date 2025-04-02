0dab7a77656cf3ac5ea95d58b5c70f4861dd7f35
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-04-02 15:12:17 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/global/Header";
 import Hero from "@/components/home/Hero";
-
+import TunnelScrollDemo from "@/nyxui/demos/tunnel-scroll-demo";
 export default function Home() {
   return (
     <>
     <Header/>
      <Hero/> 
+     <TunnelScrollDemo/>
     </>
   );
 }
