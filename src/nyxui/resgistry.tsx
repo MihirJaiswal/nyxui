306517ea1764d893109a0ece4109347040c5a1ff
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-04-07 19:09:52 +0530

    some changes

--- src/nyxui/resgistry.tsx
+++ src/nyxui/resgistry.tsx
@@ -2,15 +2,12 @@ import { gradientButtonData } from "@/nyxui/metadata/GradientButton.meta";
 import { morphingButtonData } from "@/nyxui/metadata/MorphingButton.meta";
 import { cyberpunkCardData } from "@/nyxui/metadata/cyberpunk-card.meta";
 import { dynamicRippleData } from "@/nyxui/metadata/dynamic-ripple.meta";
-import { expandingCircleMenuData } from "@/nyxui/metadata/expanding-circle-menu.meta";
 import { MajesticCardData } from "@/nyxui/metadata/majestic-card.meta";
-import { glassmorphismDepthData } from "@/nyxui/metadata/glassmorphism-depth.meta";
 import { liquidMetalButtonData } from "@/nyxui/metadata/liquid-metal-button.meta";
 import { morphingBlobData } from "@/nyxui/metadata/morphing-blob.meta";
 import { threeDCardData } from "@/nyxui/metadata/ThreeDcard.meta";
 import { animatedCodeBlockData } from "@/nyxui/metadata/AnimatedCodeBlock.meta";
 import { animatedGradientBgData } from "./metadata/AnimatedGradientBg.meta";
-import { perspectiveImageData } from "./metadata/PerspectiveImage.meta";
 import { scrollAnimationTriggerData } from "./metadata/ScrollAnimationTrigger.meta";
 import { specialCardData } from "./metadata/SpecialCard.meta";
 import { matrixCodeRainData } from "./metadata/MatrixCodeRain.meta";
@@ -26,15 +23,12 @@ export const componentsData = {
   "morphing-button": morphingButtonData,
   "cyberpunk-card": cyberpunkCardData,
   "dynamic-ripple": dynamicRippleData,
-  "expanding-circle-menu": expandingCircleMenuData,
   "majestic-card": MajesticCardData,
-  "glassmorphism-depth": glassmorphismDepthData,
   "liquid-metal-button": liquidMetalButtonData,
   "morphing-blob": morphingBlobData,
   "3d-card": threeDCardData,
   "animated-code-block": animatedCodeBlockData,
   "animated-gradient-background":animatedGradientBgData,
-  "3d-perspective-image": perspectiveImageData,
   "scroll-animation-trigger": scrollAnimationTriggerData,
   "special-card": specialCardData,
   "matrix-code-rain": matrixCodeRainData,
