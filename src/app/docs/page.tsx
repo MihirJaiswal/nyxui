63b19554381e1756f18fb13b6a6fd23a5c66fbc9
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-04-07 14:22:19 +0530

    fixed the position of docssidebar

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -8,19 +8,19 @@ const DocsPage = () => {
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
       <div className="flex w-full flex-1 flex-col md:flex-row">
-        <aside className="sticky top-16 h-fit w-full shrink-0 md:w-auto md:min-w-[220px] lg:min-w-[280px] xl:min-w-[300px] hidden md:block border-r bg-white dark:bg-zinc-950">
-          <div className="sticky top-16">
+        <aside className="hidden md:block w-full shrink-0 md:w-auto md:min-w-[220px] lg:min-w-[280px] xl:min-w-[300px] border-r bg-white dark:bg-zinc-950">
+          <div className="sticky top-16 h-[calc(100vh-4rem)] overflow-y-auto">
             <ComponentSidebar />
           </div>
         </aside>
-        <main className="flex-1 bg-white dark:bg-zinc-950 overflow-hidden">
+        <main className="flex-1 bg-white dark:bg-zinc-950">
           <div className="w-full h-full px-4 sm:px-6 md:px-8 py-4 md:py-6">
             <div className="flex flex-col lg:flex-row gap-6 lg:gap-10">
               <div className="flex-1 order-2 lg:order-1 min-w-0">
                 <IntroductionPage />
               </div>
               <div className="w-full lg:w-64 xl:w-72 shrink-0 order-1 lg:order-2">
-                <div className="sticky top-3">
+                <div className="sticky top-20 max-h-[calc(100vh-5rem)] overflow-y-auto">
                   <DocsSidebar />
                 </div>
               </div>
@@ -28,7 +28,7 @@ const DocsPage = () => {
           </div>
         </main>
       </div>
-  </div>
+    </div>
   )
 }
 
