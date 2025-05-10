beae91a76efcb0bfe20978723ea3028fe004f38f
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-05-10 19:48:04 +0530

    some changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -39,7 +39,7 @@ export function formatDate(input: string | number): string {
 }
 
 export function absoluteUrl(path: string) {
-  return `${process.env.NEXT_PUBLIC_APP_URL || ""}${path}`;
+  return `${process.env.NEXT_PUBLIC_APP_URL || "https://nuvyxui.vercel.app"}${path}`;
 }
 
 export function constructMetadata({
