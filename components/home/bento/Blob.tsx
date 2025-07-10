617de1e4fa08979ce0bbef5f6f17647c7dcb4dfd
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-07-10 15:22:37 +0530

    updated some functionalites

--- components/home/bento/Blob.tsx
+++ components/home/bento/Blob.tsx
@@ -7,11 +7,10 @@ const Blob = () => {
     <div className="w-full h-full max-w-full max-h-full overflow-hidden relative"> 
      
       <div className="flex flex-col items-center justify-center group h-full">
-        <MorphingBlob theme="cosmic">
+        <MorphingBlob theme="cosmic" className="scale-95">
         </MorphingBlob>
-        <div className="absolute -z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-72 w-66 bg-radial from-black via-purple-[#4e1674]/5 to-[rgb(53,13,58)] rounded-full blur-xl  shadow-[0_0_50px_rgba(96,165,250,0.4)]" />
-        
-        <div className="text-center z-1 w-full flex flex-col items-center justify-center gap-3 -mt-4">
+        <div className="absolute scale-95 -z-1 top-48 left-48 -translate-x-1/2 -translate-y-1/2 h-72 w-66 bg-[rgb(97,23,107)] rounded-full blur-xl shadow-[0_0_50px_rgba(96,165,250,0.4)] animate-pulse"/>
+        <div className="text-center z-1 w-full flex flex-col items-center justify-center gap-3 -mt-4 pb-12">
           <LampHeading
             text="Bulid Innovative"
             gradientColors={{ from: "#6e15ad", to: "#d413ad" }}
