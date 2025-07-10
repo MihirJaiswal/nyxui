0cd399362e98d62e592a2a49444562fa1bc6b078
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-07-10 14:28:57 +0530

    updated some functionalites

--- components/home/bento/Blob.tsx
+++ components/home/bento/Blob.tsx
@@ -9,7 +9,7 @@ const Blob = () => {
       <div className="flex flex-col items-center justify-center group h-full">
         <MorphingBlob theme="cosmic">
         </MorphingBlob>
-        <div className="absolute -z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-72 w-66 bg-radial from-black via-purple-950/5 to-purple-400 rounded-full blur-xl  shadow-[0_0_50px_rgba(96,165,250,0.4)]" />
+        <div className="absolute -z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-72 w-66 bg-radial from-black via-purple-[#4e1674]/5 to-[rgb(53,13,58)] rounded-full blur-xl  shadow-[0_0_50px_rgba(96,165,250,0.4)]" />
         
         <div className="text-center z-1 w-full flex flex-col items-center justify-center gap-3 -mt-4">
           <LampHeading
