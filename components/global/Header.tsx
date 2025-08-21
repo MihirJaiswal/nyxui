855d0e214a7fd4307a1a2e1b4f8cb35f6b04dc71
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-08-21 20:05:45 +0530

    updated navbar

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -36,6 +36,7 @@ export default function Header() {
 
   const navLinks = [
     { href: "/components", label: "Components" },
+    { href: "/templates", label: "Templates" },
     { href: "/playground", label: "Playground" },
     { href: "/docs", label: "Documentation" },
   ];
