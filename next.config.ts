6ae6cf6047ee7a8c7d58d37ea982117b712d6c70
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-09-07 23:26:58 +0530

    some optimization

--- next.config.ts
+++ next.config.ts
@@ -44,61 +44,6 @@ const nextConfig: NextConfig = withBundlerAnalyzer({
       },
     ],
   },
-  // Bundle splitting configuration
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          // React core
-          react: {
-            name: 'react-vendor',
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            chunks: 'all',
-            priority: 10,
-          },
-          // Three.js
-          threejs: {
-            name: 'three-vendor',
-            test: /[\\/]node_modules[\\/](three|@react-three)[\\/]/,
-            chunks: 'all',
-            priority: 9,
-          },
-          // Animation libraries
-          animations: {
-            name: 'animation-vendor',
-            test: /[\\/]node_modules[\\/](framer-motion|animejs|motion)[\\/]/,
-            chunks: 'all',
-            priority: 8,
-          },
-          // Firebase
-          firebase: {
-            name: 'firebase-vendor',
-            test: /[\\/]node_modules[\\/]firebase[\\/]/,
-            chunks: 'all',
-            priority: 7,
-          },
-          // UI libraries
-          ui: {
-            name: 'ui-vendor',
-            test: /[\\/]node_modules[\\/](@radix-ui|lucide-react)[\\/]/,
-            chunks: 'all',
-            priority: 6,
-          },
-          // Other vendors
-          vendor: {
-            name: 'vendor',
-            test: /[\\/]node_modules[\\/]/,
-            chunks: 'all',
-            priority: 5,
-            minChunks: 1,
-          },
-        },
-      };
-    }
-    
-    return config;
-  },
 });
 
 export default withContentCollections(withPlaiceholder(nextConfig));
