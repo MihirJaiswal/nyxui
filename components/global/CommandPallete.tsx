21e8947335f8cb863fa7a9053becee461d15eeef
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2026-07-27 22:37:59 +0530

    some changes

--- components/global/CommandPallete.tsx
+++ components/global/CommandPallete.tsx
@@ -20,7 +20,7 @@ import {
   CommandItem,
   CommandList,
   CommandSeparator,
-} from "./Command";
+} from "@/components/ui/command";
 import { Button } from "../ui/button";
 import {
   componentsData,
