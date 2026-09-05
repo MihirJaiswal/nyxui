69c5c80afece1ec3f64e81a6931a6a2a26c7c4c0
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2026-09-05 11:26:54 +0530

    some image changes

--- components/home/bento/ImageRipple.tsx
+++ components/home/bento/ImageRipple.tsx
@@ -5,12 +5,12 @@ function Imageripple() {
     <div className=" flex items-center justify-center">
       <div className="flex w-full items-center justify-center">
         <WaterRippleEffect
-          imageSrc="/assets/images/landing-page/imgd.png"
+          imageSrc="/assets/images/landing-page/img.avif"
           rippleIntensity={0}
           hoverRippleMultiplier={50}
           height={215}
           width={300}
-          className="!p-0"
+          className="p-0!"
         />
       </div>
     </div>
